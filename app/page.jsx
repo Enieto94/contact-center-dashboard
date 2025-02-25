d676333715efc8fe8a0d99050f6446f28c354320
Edwin Nieto <enietobaron@uniminuto.edu.co>
2025-02-25 02:19:04 -0500

    imports FiltroClientes and FiltroAgentes removed from page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,6 @@ import { ContactCenterProvider } from "@/context/ContactCenterContext";
 import Menu from "@/components/Menu";
 import ClientesPage from "./clientes/page";
 import AgentesPage from "./agentes/page";
-import FiltroClientes from "@/components/FiltroClientes";
-import FiltroAgentes from "@/components/FiltroAgentes";
 
 export default function Home() {
   return (
